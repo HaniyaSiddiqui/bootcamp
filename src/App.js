e71486e988751ff65373c219944a1ca92ff90b28
HaniyaSiddiqui <haniya.siddiqui124@gmail.com>
2021-02-14 20:51:09 +0500

    React use of props and setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
+import react,{useState} from 'react';
+import {Message} from './Message.js';
 import './App.css';
 
-function App() {
+export default function App() {
+
+  let [count,setCount] = useState(1);
+  let[isMorning, setMorning] =useState(true)
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Hello ....M...E....</h1>
-      </header>
+    <div className={`box ${ isMorning ? 'dayLight' : '' }`}>
+      <h1> Day Time = { isMorning ? 'Morning' : 'Night' }</h1>
+      <Message counter={count} />
+      {/* <h1> The value of count is {count}</h1> */}
+      <button onClick={
+        () => setCount(++count)
+      } > Update Counter</button>
+   
+      <button onClick={
+        () => setMorning(!isMorning)
+      }> Update Day</button>
+
     </div>
   );
 }
-
-export default App;
